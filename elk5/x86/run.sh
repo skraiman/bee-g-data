fe23c43749defbcb6314d6be3844259a187fa764
Kraiman,Stephen <stephen.kraiman@arris.com>
2017-01-22 16:43:48 -0500

    - run.sh - fixed creation of directory

--- elk5/x86/run.sh
+++ elk5/x86/run.sh
@@ -3,5 +3,7 @@
 # store the elasticsearch data locally in ./elasticsearch-data so it persists across 
 # the creation and destruction of the docker instances.
 #
-docker run --name elastic -p 9200:9200  -v "$PWD/elasticsearch-data":/usr/share/elasticsearch/data -d  elasticsearch:5.1.2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run --name elastic -p 9200:9200  -v "$DIR/elasticsearch-data":/usr/share/elasticsearch/data -d  elasticsearch:5.1.2
 docker run --name kibana --link elastic:elasticsearch -p 5601:5601 -d kibana:5.1.2
